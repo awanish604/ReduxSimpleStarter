fbeeaea6ef0bba75aea25d80e8fd2b0214dd5627
Awanish <awanish.604@gmail.com>
2018-06-24 23:30:51 +0530

    initial commit with the input box of search box

---
+++ src/components/search_bar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Searchbar= () => {
+    return(
+    <div>
+        <input/>
+    </div>
+    
+    )
+}
+
+export default Searchbar
--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import Searchbar from './components/search_bar'
 
-import App from './components/app';
+
 import reducers from './reducers';
+
+const youtubeApi= 'AIzaSyBMQ4UCBb8lRJVjjvRpKlEM3LBJqHdwi-Q'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  
+    <Searchbar />
   </Provider>
   , document.querySelector('.container'));
